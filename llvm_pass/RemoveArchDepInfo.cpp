5e24d6b91d996620a87678cc372adec25809ad27
srihari <datta.srihari@gmail.com>
2023-10-19 14:46:47 -0400

    updated make file

--- llvm_pass/RemoveArchDepInfo.cpp
+++ llvm_pass/RemoveArchDepInfo.cpp
@@ -9,32 +9,37 @@
 using namespace llvm;
 
 namespace {
-struct RemoveArchDepInfo : public PassInfoMixin<RemoveArchDepInfo> {
-  PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
-    F.getParent()->setTargetTriple("x86_64-unknown-linux-gnu");
-    F.getParent()->setDataLayout("e-m:e-i64:64-f80:128-n8:16:32:64-S128");
-    bool Changed = false;
-    // Remove inline asm
-    for (auto &BB : F) {
-      for (auto &I : BB) {
-        if (auto *CI = dyn_cast<CallInst>(&I)) {
-          if (CI->isInlineAsm()) {
-            outs() << "Remove inline asm: " << *CI << "\n";
-            //CI->removeFromParent();
-            Changed = true;
+  struct RemoveArchDepInfo : public PassInfoMixin<RemoveArchDepInfo> {
+    PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
+
+      // Update the target triple and data layout to match the x86_64 target.
+      F.getParent()->setTargetTriple("x86_64-unknown-linux-gnu");
+      F.getParent()->setDataLayout("e-m:e-i64:64-f80:128-n8:16:32:64-S128");
+
+      // Remove inline asm
+      for (auto &BB : F) {
+        for (auto I = BB.begin(); I != BB.end(); /* no increment here */) {
+          Instruction *currentInstruction = &*I;
+          if (auto *CI = dyn_cast<CallInst>(currentInstruction)) {
+            if (CI->isInlineAsm()) {
+              outs() << "Remove inline asm: " << *CI << "\n";
+              // Erase the instruction from the basic block and increment the iterator.
+              I = CI->eraseFromParent();
+              continue;
+            }
           }
+          // Move to the next instruction in the basic block.
+          ++I;
         }
       }
-    }
-    if (Changed) {
+
       return PreservedAnalyses::none();
-    } else {
-      return PreservedAnalyses::all();
     }
-  }
-};
+  };
 }
 
+// Register the pass with the pass manager builder.  This instructs the
+// builder to call the `run` function on your pass when it is constructing
 extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK
 llvmGetPassPluginInfo() {
   return {
